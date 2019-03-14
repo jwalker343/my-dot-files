ddecc2e062ec70077ebb4fab5d0fc990f480bf31
Johnny Walker <jwalker343@users.noreply.github.com>
2019-03-13 22:52:27 -0400

    add line to pause for xcode gui install

--- install.sh
+++ install.sh
@@ -22,6 +22,8 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 sudo softwareupdate -i -a
 xcode-select --install
 
+read -rsp $'Finish the xcode GUI install and then press any key to continue...\n' -n1 key
+
 # Clone in the repo and execute bootstrap
 cd; git clone "https://github.com/jwalker343/my-dot-files.git" "$DOTFILE_GIT_DIRECTORY/my-dot-files/"
 bash -c "$DOTFILE_GIT_DIRECTORY/my-dot-files/bootstrap.sh"
