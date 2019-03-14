4243d5aecc49b0473e5fec3b73a85bce802e2f55
Johnny Walker <jwalker343@users.noreply.github.com>
2019-03-14 19:20:43 -0400

    lastpass was removed

--- setup/addons.sh
+++ setup/addons.sh
@@ -3,6 +3,3 @@
 # Install Addons
 sudo gem install rubocop
 helm plugin install https://github.com/databus23/helm-diff
-
-# Install Lastpass addons
-open '/usr/local/Caskroom/lastpass/latest/LastPass Installer'
