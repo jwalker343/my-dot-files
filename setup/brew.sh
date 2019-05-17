0daa7f45f1dff6e0c4eb293c01db00e189262b19
Johnny Walker <johnny.walker@spinen.com>
2019-05-17 11:25:44 -0400

    alphabetize

--- setup/brew.sh
+++ setup/brew.sh
@@ -6,44 +6,44 @@
 # Install CLI tools from Brew
 
 # General
-brew install gpg                        # Encrypt all the things
 brew install bash-completion            # Because lazy
-brew install watch                      # Watch a command output over time
+brew install gpg                        # Encrypt all the things
 brew install parallel                   # GNU Parallel / execute jobs in parallel
 brew install tmux                       # Terminal Multiplexer
+brew install watch                      # Watch a command output over time
 
 # Developer
 brew install git                        # Source Control
+brew install cfssl                      # Cloudflare's PKI/TLS toolkit
+brew install diff-so-fancy              # Better diff tolls for GIT
 brew install git-crypt                  # Encrypt files in git
 brew install git-flow                   # Better git methodoligies
-brew install diff-so-fancy              # Better diff tolls for GIT
-brew install node                       # node
-brew install yarn                       # yarn for node
 brew install jq                         # Process JSON files in bash
-brew install cfssl                      # Cloudflare's PKI/TLS toolkit
+brew install node                       # node
 brew install shellcheck                 # Lint bash scripts
+brew install yarn                       # yarn for node
 
 # Kubernetes
 brew install kubernetes-cli             # Kubernetes
 brew install kubernetes-helm            # Kubernetes Helm Client
 brew install helmfile                   # Declarative Helm Installations
-brew install kustomize                  # Structured kubernetes yaml files
 brew install kubectx                    # Interactive kube context switching
+brew install kustomize                  # Structured kubernetes yaml files
 
 # Tech Tools
+brew install ipcalc                     # CLI IP/subnet calc
 brew install nmap                       # Network scanning tool
 brew install speedtest_cli              # Speed test from CLI
-brew install ipcalc                     # CLI IP/subnet calc
 brew install telnet                     # telnet client
 
 # Cloud
-brew install azure-cli                  # Azure Command Line Client
 brew install awscli                     # AWS Command Line Client
+brew install azure-cli                  # Azure Command Line Client
 
 # Awesomeness
+brew install fzf                        # fuzzy completion
+brew install hr                         # <hr /> for terminal
+brew install httpie                     # Curl but with colors!
 brew install thefuck                    # Automatically Fix Errors
 brew install tldr                       # Shorter Man Pages
 brew install z                          # Shortcut for recent Dirs
-brew install hr                         # <hr /> for terminal
-brew install httpie                     # Curl but with colors!
-brew install fzf                        # fuzzy completion
--- setup/casks.sh
+++ setup/casks.sh
@@ -2,14 +2,14 @@
 
 # Taps
 brew tap caskroom/cask                              # Cask can install applications
-brew tap caskroom/versions                          # Cask can install Older Versions
 brew tap caskroom/fonts                             # Cask can install Fonts
+brew tap caskroom/versions                          # Cask can install Older Versions
 brew tap chef/chef                                  # Chefdk is hosted on a seperate tap
 
 # Browsers
+brew cask install epichrome                         # Make websites like apps
 brew cask install firefox                           # Because safari sucks
 brew cask install google-chrome                     # Because firefox sucks
-brew cask install epichrome                         # Make websites like apps
 
 # Docker
 brew cask install docker                            # Containerization Software
@@ -24,17 +24,16 @@ brew cask install quicklook-csv                     # CSV Files
 brew cask install quicklook-json                    # JSON Files
 
 # General
-brew cask install azure-data-studio                 # Like MySQL Workbench for sql servers
-brew cask install google-cloud-sdk                  # gcloud SDK
 brew cask install angry-ip-scanner                  # Scan Local IP Networks
+brew cask install azure-data-studio                 # Like MySQL Workbench for sql servers
 brew cask install balenaetcher                      # Bootable USB drive Maker
 brew cask install bettertouchtool                   # Customize mouse, keyboard, touchbar
 brew cask install caffeine                          # Keeps your screen awake
 brew cask install cheatsheet                        # Hold Command to get Shortcuts
 brew cask install chefdk                            # Chef, Knife toolset
-brew cask install webex-teams                       # Collaboration & Messaging
 brew cask install font-fira-code                    # Font with Ligatures
 brew cask install google-backup-and-sync            # Google Drive Replacement
+brew cask install google-cloud-sdk                  # gcloud SDK
 brew cask install java8                             # Unfortunately, java
 brew cask install kaleidoscope                      # Diff tool
 brew cask install microsoft-remote-desktop-beta     # Because Windows
@@ -43,5 +42,6 @@ brew cask install skitch                            # Capture & Annotate your sc
 brew cask install spotify                           # Amazing Music
 brew cask install timing                            # Time Tracking App
 brew cask install visual-studio-code-insiders       # code editor
+brew cask install webex-teams                       # Collaboration & Messaging
 brew cask install xquartz                           # x11 forwarding thingy for mac
 brew cask install zterm                             # Cisco Console Connections
